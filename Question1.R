d1e5e8ad3c05820fd160910a543a22e60d5b2c49
Kyle Sommerfield <ksommerf@nd.edu>
2019-11-03 13:32:51 -0400

    answer to question 1

---
+++ Question1.R
@@ -0,0 +1,5 @@
+#Kyle Sommerfield and Farai Musariri
+#Return a defined number of lines from the beginning of a file
+file <- file to return lines from
+n <- number of desired lines
+read.csv(file="file", nrows=n)
