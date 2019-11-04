48a999fd0a302d25471aec1f9271bf5129f849ce
Kyle Sommerfield <ksommerf@nd.edu>
2019-11-04 16:25:14 -0400

    update to question 2

--- Question2.R
+++ Question2.R
@@ -3,8 +3,15 @@
 iris <- read.csv(file="iris.csv")
 iris[149:150, 4:5]
 
+#Count number of observations for each species
+table(iris$Species)
+
 #Print rows with Sepal.Width>3.5
 subset(iris, Sepal.Width>3.5)
+
+#Create comma-delimited file for setosa
+setosa <- iris[1:50,]
+write.csv(setosa,file="setosa.csv",row.names=FALSE,na="")
 
 #Print mean, min, and max of Petal.Length for virginica
 mean(iris[101:150,3])
