051cea1fe79b7b3b357352b1280a99fe95f0c320
Kyle Sommerfield <ksommerf@nd.edu>
2019-11-04 11:18:40 -0400

    Updated Question 2

--- Question2.R
+++ Question2.R
@@ -1,4 +1,12 @@
 #Kyle Sommerfield and Farai Musariri
 #Print last two rows of last two columns
 iris <- read.csv(file="iris.csv")
-iris[149:150,4:5]
+iris[149:150, 4:5]
+
+#Print rows with Sepal.Width>3.5
+subset(iris, Sepal.Width>3.5)
+
+#Print mean, min, and max of Petal.Length for virginica
+mean(iris[101:150,3])
+max(iris[101:150,3])
+min(iris[101:150,3])
